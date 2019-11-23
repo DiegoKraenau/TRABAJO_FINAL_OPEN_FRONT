e06f128cfd6d9e15f0b5140279aa510e3089d955
DiegoKraenau <diegokraenau@gmail.com>
2019-11-23 02:55:45 -0500

    aggasg

--- rapiSolverFront/src/app/login-usuario/login-usuario.component.ts
+++ rapiSolverFront/src/app/login-usuario/login-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UsuarioService } from '../usuario.service';
+import { Usuario } from '../model/usuario';
 
 @Component({
   selector: 'app-login-usuario',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginUsuarioComponent implements OnInit {
 
-  constructor() { }
+  usuario =new Usuario();
+  usuariosGroup:Usuario[];
+
+  constructor(private route: ActivatedRoute,private usuarioService: UsuarioService) { }
 
   ngOnInit() {
   }
 
+  logear(){
+    var num=0;
+    var codigo;
+    this.usuarioService.getCategoriesList().subscribe(
+      usuariosGroup=>{
+        this.usuariosGroup=usuariosGroup;
+        usuariosGroup.forEach(element => {
+           if(element.correo==this.usuario.correo && element.userPassword==this.usuario.userPassword){
+            num=1;codigo=element.id;
+           }
+        });
+        if (num==1) {
+          window.location.href="listarServicios/"+codigo
+        }
+        if (num==0) {
+          document.getElementById('mensaje2').style.display ='block'
+          document.getElementById('mensaje').innerHTML = 'Correo electronico o contraseña incorrectos.';
+        }
+      }
+
+    );
+
+  }
 }
